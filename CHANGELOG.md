deee828a5ff2b4b59f5a49ca133348a74d875259
Tobias Davis <tobias@davistobias.com>
2022-01-26 17:09:34 -0600

    init: copy from saibotsivad/init

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+Change categories are:
+
+* `Added` for new features.
+* `Changed` for changes in existing functionality.
+* `Deprecated` for once-stable features removed in upcoming releases.
+* `Removed` for deprecated features removed in this release.
+* `Fixed` for any bug fixes.
+* `Security` to invite users to upgrade in case of vulnerabilities.
+
+## Unreleased
+### Added
+### Changed
+### Deprecated
+### Fixed
+### Removed
+### Security
+
+## [0.0.0](https://github.com/saibotsivad/mongodb-local-data-api/tree/v0.0.0) - 2021-01-26
+### Added
+- Created the base project from [saibotsivad/init](https://github.com/saibotsivad/init).
+
+[0.0.1]: https://github.com/saibotsivad/mongodb-local-data-api/compare/v0.0.0...v0.0.1
---
+++ bin.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+import { readFileSync } from 'node:fs'
+import sade from 'sade'
+import Koa from 'koa'
+import koaRouter from 'koa-router'
+import koaBody from 'koa-body'
+import { setup } from './src/index.js'
+
+const optionNames = [
+	'apiPort',
+	'url',
+	'username',
+	'password',
+	'dbDomain',
+	'dbPort',
+]
+
+const mixWithEnv = callback => options => {
+	const envOptions = {}
+	for (const envName in process.env) {
+		const tidyName = envName.toUpperCase()
+		if (tidyName.startsWith('MONGODBLOCAL_')) envOptions[tidyName] = process.env[envName]
+	}
+	for (const opt of optionNames) {
+		if (options[opt] === undefined && envOptions[`MONGODBLOCAL_${opt.toUpperCase()}`]) {
+			options[opt] = envOptions[`MONGODBLOCAL_${opt.toUpperCase()}`]
+		}
+	}
+	if (options.apiPort) options.apiPort = parseInt(options.apiPort, 10)
+	if (options.dbPort) options.dbPort = parseInt(options.dbPort, 10)
+	callback(options)
+}
+
+sade('mongodb-local-data-api', true)
+	.version(JSON.parse(readFileSync('./package.json', 'utf8')).version)
+	.describe('Start an instance of a Data API server. Use environment variables instead by prefixing `MONGODBLOCAL_` to the option, e.g. `MONGODBLOCAL_PORT` for `port`.')
+	.option('--apiPort', 'Change the port this Data API uses. (Default: 3007)')
+	.option('--url', 'The complete MongoDB URL, e.g. `mongodb://AzureDiamond:hunter2@localhost:27017`. If set, this will override all other related properties.')
+	.option('--username', 'The MongoDB username, e.g. `AzureDiamond`.')
+	.option('--password', 'The MongoDB password, e.g. `hunter2`.')
+	.option('--dbDomain', 'Change the port used to connect to MongoDB.', 'localhost')
+	.option('--dbPort', 'Change the port used to connect to MongoDB. (Default: 27017)')
+	.option('--key', 'The API key to require on all requests. The default is no authentication required. Set the flag multiple times for multiple keys.')
+	.option('--verbose', 'If this flag is set, all request and response bodies will also be logged.')
+	.example('build src build --global --config my-conf.js')
+	.example('build app public -o main.js')
+	.action(mixWithEnv(({ apiPort, url, username, password, dbDomain, dbPort, key: keys, verbose }) => {
+		if (!Array.isArray(keys)) keys = keys ? [ keys ] : []
+		if (!apiPort) apiPort = 3007
+		if (!dbPort) dbPort = 27017
+		if (!url) url = `mongodb://${username}:${password}@${dbDomain}:${dbPort}`
+		const database = setup({ url })
+		const app = new Koa()
+		const router = koaRouter()
+		let requestCounter = 0
+		router
+			.post('/action/:action', koaBody(), async context => {
+				const action = context.request.params.action
+				const requestId = (++requestCounter).toString().padStart(4, '0')
+				console.log(requestId, new Date(), action)
+				if (verbose) console.log(context.request.body)
+				if (keys.length && !keys.includes(context.request.headers['api-key'])) {
+					console.log(requestId, new Date(), 'Provided API key was not authorized:', context.request.headers['api-key'])
+					context.status = 401
+					context.body = {
+						error: 'invalid session: error finding user for endpoint',
+						error_code: 'InvalidSession',
+						link: 'https://realm.mongodb.com/groups/611be2cf39b99f2a25556d10/apps/61df4d50cb330d69cb2b7822/logs?co_id=61f1c4aaa37cb8585dbdd69c',
+					}
+				} else {
+					await database(action, context.request.body).then(({ status, body }) => {
+						context.status = status
+						context.body = body
+						console.log(requestId, new Date(), `${action} => ${status}`)
+						if (verbose) console.log(body)
+					})
+				}
+			})
+		app.use(router.routes())
+		app.listen(apiPort, () => {
+			console.log(new Date(), `Data API running on port ${apiPort}`)
+		})
+	}))
+	.parse(process.argv)
